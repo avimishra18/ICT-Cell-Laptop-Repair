d48f42e712a2ac8793fba4fc0087da4211d4dc39
avimishra18 <1828058@kiit.ac.in>
2019-03-18 10:17:07 +0530

    Register & Log In Front End Commit

--- app/src/main/java/com/example/ictcelllaptoprepair/LoginActivity.java
+++ app/src/main/java/com/example/ictcelllaptoprepair/LoginActivity.java
@@ -1,23 +1,32 @@
 package com.example.ictcelllaptoprepair;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
+    //Declaring Login Edit Text & Button
+    EditText usernameEditText, passwordEditText;
+    Button loginButton, registerSwitchButton;
+
+    //Basic Splash Screen + Login Activity
     RelativeLayout layoutRelayOne,layoutRelayTwo;
     Handler handler = new Handler();
     Runnable runnable = new Runnable() {
         @Override
         public void run() {
+            //Sets Visibility when the code will be run
             layoutRelayOne.setVisibility(View.VISIBLE);
             layoutRelayTwo.setVisibility(View.VISIBLE);
-
         }
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +34,23 @@ public class LoginActivity extends AppCompatActivity {
 
         layoutRelayOne = findViewById(R.id.relativeOne);
         layoutRelayTwo = findViewById(R.id.relativeTwo);
-
+        //Sets the delay
         handler.postDelayed(runnable,2500);
+
+
+        //Linking the xml with Java file
+        usernameEditText = findViewById(R.id.editTextUserName);
+        passwordEditText = findViewById(R.id.editTextPassword);
+        loginButton = findViewById(R.id.buttonLogin);
+        registerSwitchButton = findViewById(R.id.buttonSignUp);
+
+        registerSwitchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/ictcelllaptoprepair/RegisterActivity.java
@@ -0,0 +1,37 @@
+package com.example.ictcelllaptoprepair;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    //Declaring Register Edit Text & Buttons
+    EditText registerUserName, registerNumber, registerPassword,registerRePassword;
+    Button registerButton, loginSwitchButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        registerUserName = findViewById(R.id.editTextUserName);
+        registerNumber = findViewById(R.id.editTextNumber);
+        registerPassword = findViewById(R.id.editTextPassword);
+        registerRePassword = findViewById(R.id.editTextRePassword);
+        registerButton = findViewById(R.id.buttonSignUp);
+        loginSwitchButton = findViewById(R.id.buttonLogin);
+
+        loginSwitchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
